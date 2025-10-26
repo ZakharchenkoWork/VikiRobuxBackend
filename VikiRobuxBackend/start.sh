a467d5ca797944d78bf1ef9338494a017e83c468
Konstantyn <k.zakharchenko@wittix.com>
2025-10-26 17:42:58 +0200

    Add docker file

---
+++ VikiRobuxBackend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# переходим в папку, где лежит build.gradle.kts
+cd VikiRobuxBackend
+
+# собираем "fat jar" (всё в одном файле)
+./gradlew fatJarCustom --no-daemon
+
+# запускаем сервер
+java -jar build/libs/*-all.jar
